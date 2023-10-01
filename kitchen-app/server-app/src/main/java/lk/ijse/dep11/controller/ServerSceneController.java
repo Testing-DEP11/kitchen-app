f809b82ab311bc40ac9600e379d0633b5412aad2
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-10-01 17:19:26 +0530

    Complete server UI

---
+++ kitchen-app/server-app/src/main/java/lk/ijse/dep11/controller/ServerSceneController.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep11.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class ServerSceneController {
+    public AnchorPane root;
+    public TableView tblOrders;
+    public Button btnCloseOrder;
+
+    public void btnCloseOrderOnAction(ActionEvent actionEvent) {
+    }
+}
