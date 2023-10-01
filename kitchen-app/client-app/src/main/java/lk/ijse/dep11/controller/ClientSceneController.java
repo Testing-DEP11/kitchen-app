c11bc0bcf92eba5bf8e0327757b53dfaa588aa1f
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-10-01 14:56:18 +0530

    Complete client UI

---
+++ kitchen-app/client-app/src/main/java/lk/ijse/dep11/controller/ClientSceneController.java
@@ -0,0 +1,28 @@
+package lk.ijse.dep11.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientSceneController {
+    public AnchorPane root;
+    public TableView tblCustomerDetail;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContact;
+    public Button btnNew;
+    public Spinner spnBurger;
+    public Spinner spnSubmarine;
+    public Spinner spnHotDog;
+    public Spinner spnCoke;
+    public Button btnPlaceOrder;
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrder(ActionEvent actionEvent) {
+    }
+}
